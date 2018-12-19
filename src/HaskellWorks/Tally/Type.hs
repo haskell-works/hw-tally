4f50afea6ed9140862ef509c3d79b020abf9ec56
John Ky <newhoggy@gmail.com>
2018-12-19 08:10:51 -0800

    FromJSON and ToJSON instances

--- src/HaskellWorks/Tally/Type.hs
+++ src/HaskellWorks/Tally/Type.hs
@@ -1,31 +1,33 @@
+{-# LANGUAGE DeriveAnyClass        #-}
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 
 module HaskellWorks.Tally.Type where
 
+import Data.Aeson
 import Data.Map     (Map)
 import Data.Set     (Set)
 import Data.Text    (Text)
 import GHC.Generics
 
-data Gender = Female | NonFemale deriving (Eq, Ord, Show, Generic)
+data Gender = Female | NonFemale deriving (Eq, Ord, Show, Generic, ToJSON, FromJSON)
 
 type CandidateName = Text
 
 newtype CandidateInfo = CandidateInfo
   { gender :: Gender
-  } deriving (Eq, Ord, Show, Generic)
+  } deriving (Eq, Ord, Show, Generic, ToJSON, FromJSON)
 
 data Vote = Vote
   { value       :: Double
   , preferences :: [CandidateName]
   , spent       :: Map CandidateName Double
   , history     :: [Text]
-  } deriving (Eq, Show, Generic)
+  } deriving (Eq, Show, Generic, ToJSON, FromJSON)
 
 newtype Distributions = Distributions
   { votesByCandidate :: Map CandidateName [Vote]
-  } deriving (Eq, Show, Generic)
+  } deriving (Eq, Show, Generic, ToJSON, FromJSON)
 
 data Election = Election
   { candidates :: Map CandidateName CandidateInfo
@@ -37,5 +39,4 @@ data Election = Election
   , results    :: [CandidateName]
   , round      :: Int
   , history    :: [Text]
-  , earlier    :: Election
-  } deriving (Eq, Show, Generic)
+  } deriving (Eq, Show, Generic, ToJSON, FromJSON)
